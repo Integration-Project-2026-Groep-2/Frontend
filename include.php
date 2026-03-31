685110879b945efb0008bdd5a57409cef9baf47c
LiamStammeleerEhb <liam.stammeleer@student.ehb.be>
2026-03-31 21:58:35 +0200

    Making the .env under root optional

--- include.php
+++ include.php
@@ -1,6 +1,12 @@
 <?php
+
 	require_once __DIR__.'/vendor/autoload.php';
 	use Dotenv\Dotenv;
-	$dotenv = Dotenv::createImmutable(__DIR__);
-	$dotenv->load();
+
+	if (is_file(__DIR__.'/.env')) {
+		
+		$dotenv = Dotenv::createImmutable(__DIR__);
+		$dotenv->load();
+	}
+	
 	include 'private/include.php';
