4e2ab3e14e7b76f0f62d24d9657a6eba1e5096b3
AbdennourAFK <abdennour.afkir@student.ehb.be>
2026-04-08 01:56:31 +0200

    voeg bedrijfsgegevens pagina toe

---
+++ bedrijf_registratie/src/Controller/RegistratieController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\bedrijf_registratie\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class RegistratieController extends ControllerBase {
+
+  public function gegevensPagina() {
+    // Deze variabelen gaan we later vullen met echte data uit de database.
+    $bedrijfsnaam = "Bedrijf X"; 
+    $ondernemingsnummer = "BE 0123.456.789";
+    $email = "info@bedrijfx.be";
+    $telefoon = "+32 470 00 00 00";
+    $zetel = "straat 1, 1000 Brussel";
+
+    $output = [
+      '#type' => 'markup',
+      '#markup' => '<h2>' . $this->t('Uw Bedrijfsgegevens') . '</h2>' .
+                   '<ul>' .
+                   '<li><strong>' . $this->t('Bedrijfsnaam') . ':</strong> ' . $bedrijfsnaam . '</li>' .
+                   '<li><strong>' . $this->t('Ondernemingsnummer') . ':</strong> ' . $ondernemingsnummer . '</li>' .
+                   '<li><strong>' . $this->t('E-mailadres') . ':</strong> ' . $email . '</li>' .
+                   '<li><strong>' . $this->t('Telefoonnummer') . ':</strong> ' . $telefoon . '</li>' .
+                   '<li><strong>' . $this->t('Maatschappelijke zetel') . ':</strong> ' . $zetel . '</li>' .
+                   '</ul>',
+    ];
+    return $output;
+  }
+
+}
