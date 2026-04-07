54d498b628b0092b415160cab8114229e130641b
Jelle schroeven <jelle.schroeven@student.ehb.be>
2026-04-07 15:54:40 +0200

    Admin dashboard  base layout
    A simple page layout for everything that we are expecting on the page

--- modules/custom/module/src/Controller/AdminController.php
+++ modules/custom/module/src/Controller/AdminController.php
@@ -10,16 +10,45 @@ class AdminController extends ControllerBase {
         return [
             '#type' => 'inline_template',
             '#template' => '
-                <p>Dit is de admin pagina. Hier kan je zaken zoals sessies beheren, gebruikers bekijken en inschrijvingen opvolgen.</p>
+                <div class="admin-home">
+                    
+                    <p>Dit is de admin pagina. Hier kan je zaken zoals sessies beheren, gebruikers bekijken en inschrijvingen opvolgen.</p>
 
-                <nav>
-                    <ul>
-                        <li><a href="/admin/hello-world/sessies">Sessies</a></li>
-                        <li><a href="/admin/hello-world/gebruikers">Gebruikers</a></li>
-                        <li><a href="/admin/hello-world/inschrijvingen">Inschrijvingen</a></li>
-                        <li><a href="/admin/hello-world/feedback">Feedback</a></li>
-                    </ul>
-                </nav>
+                    <div class="admin-cards">
+
+                        <div class="admin-card">
+                            <nav>
+                                <ul>
+                                    <li><a href="/admin/hello-world/sessies">Sessies</a></li>
+                                    <li><a href="/admin/hello-world/gebruikers">Gebruikers</a></li>
+                                    <li><a href="/admin/hello-world/inschrijvingen">Inschrijvingen</a></li>
+                                    <li><a href="/admin/hello-world/feedback">Feedback</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+
+                        <div class="admin-card">
+                            <h2> Bedrijven </h2>
+                        </div>
+
+                        <div class="admin-card">
+                            <h2> Bezoekers </h2>
+                        </div>
+
+                        <div class="admin-card">
+                            <h2> Sessies </h2>
+                        </div>
+
+                        <div class="admin-card">
+                            <h2> Feedback </h2>
+                        </div>
+
+                        <div class="admin-instellingen">
+                            <a href="/hello-admin/instellingen">⚙️ Instellingen</a>
+                        </div>
+
+                    </div>
+                </div>
             ',
             '#context' => [],
         ];
