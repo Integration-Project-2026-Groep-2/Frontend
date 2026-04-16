a0efe3e992138e4cef11db596bcbcc58e951cf88
Jelle schroeven <jelle.schroeven@student.ehb.be>
2026-04-16 11:55:57 +0200

    Add company registration form with RabbitMQ
    Introduce RegisterCompanyForm (modules/custom/module/src/Form/RegisterCompanyForm.php) providing a Drupal form to register companies. The form collects contact and company info (name, VAT, address, contact person, GDPR consent) with basic validation (phone must be >=10 digits, KvK must be 8 digits) and a default country of 'NL'. On submit it builds a CompanyCreated XML payload and publishes it to RabbitMQ via PhpAmqpLib using environment vars (RABBITMQ_HOST, RABBITMQ_USER, RABBITMQ_PASS); the message is sent as persistent (delivery_mode=2). Adds a user-facing success message after publishing.

---
+++ modules/custom/module/src/Form/RegisterCompanyForm.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace Drupal\hello_world\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class RegisterCompanyForm extends FormBase {
+
+  public function getFormId(): string {
+    return 'register_company_form';
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $form['contact_person'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Contact person information'),
+    ];
+
+    $form['contact_person']['contact_first_name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Contact person first name'),
+      '#required' => TRUE,
+    ];
+
+    $form['contact_person']['contact_last_name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Contact person last name'),
+      '#required' => TRUE,
+    ];
+
+    $form['contact_person']['email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Email'),
+      '#required' => TRUE,
+    ];
+
+    $form['contact_person']['phone'] = [
+      '#type' => 'tel',
+      '#title' => $this->t('Contact person phone number'),
+      '#required' => FALSE,
+    ];
+
+    $form['company'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Company information'),
+    ];
+
+    $form['company']['company_name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Company Name'),
+      '#required' => TRUE,
+    ];
+
+    $form['company']['vat_number'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('VAT Number'),
+      '#required' => FALSE,
+    ];
+
+    $form['company']['street'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Street'),
+      '#required' => TRUE,
+    ];
+
+    $form['company']['house_number'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('House Number'),
+      '#required' => TRUE,
+    ];
+
+    $form['company']['postal_code'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Postal Code'),
+      '#required' => TRUE,
+    ];
+
+    $form['company']['city'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('City'),
+      '#required' => TRUE,
+    ];
+
+    $form['company']['country'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Country'),
+      '#default_value' => 'NL',
+      '#required' => TRUE,
+    ];
+
+    $form['gdpr_consent'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('I agree to the GDPR terms'),
+      '#required' => TRUE,
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Register Company'),
+    ];
+
+    return $form;
+  }
+  
+  public function validateForm(array &$form, FormStateInterface $form_state): void {
+    $phone = (string) $form_state->getValue('phone');
+    if ($phone !== '' && strlen(preg_replace('/\D/', '', $phone)) < 10) {
+      $form_state->setErrorByName('phone', $this->t('Phone number must be at least 10 digits.'));
+    }
+
+    $kvk = preg_replace('/\D/', '', (string) $form_state->getValue('kvk_number'));
+    if (strlen($kvk) !== 8) {
+      $form_state->setErrorByName('kvk_number', $this->t('KvK number must contain 8 digits.'));
+    }
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
+    $xml = new \SimpleXMLElement('<CompanyCreated/>');
+    $xml->addChild('name', (string) $form_state->getValue('company_name'));
+    $xml->addChild('vatNumber', (string) $form_state->getValue('vat_number'));
+    $xml->addChild('email', (string) $form_state->getValue('email'));
+
+    $phone = (string) $form_state->getValue('phone');
+    if ($phone !== '') {
+      $xml->addChild('phone', $phone);
+    }
+
+    $xml->addChild('street', (string) $form_state->getValue('street'));
+    $xml->addChild('houseNumber', (string) $form_state->getValue('house_number'));
+    $xml->addChild('postalCode', (string) $form_state->getValue('postal_code'));
+    $xml->addChild('city', (string) $form_state->getValue('city'));
+    $xml->addChild('country', (string) $form_state->getValue('country'));
+
+    $connection = new AMQPStreamConnection(
+      $_ENV['RABBITMQ_HOST'] ?? '',
+      5672,
+      $_ENV['RABBITMQ_USER'] ?? '',
+      $_ENV['RABBITMQ_PASS'] ?? ''
+    );
+
+    $channel = $connection->channel();
+
+    $exchange = '';
+    $routing_key = '';
+
+    $msg = new AMQPMessage($xml->asXML(), [
+      'content_type' => 'text/xml',
+      'delivery_mode' => 2,
+    ]);
+
+    $channel->basic_publish($msg, $exchange, $routing_key);
+
+    $channel->close();
+    $connection->close();
+
+    $this->messenger()->addStatus($this->t('Company registration successful!'));
+  }
+
+}
