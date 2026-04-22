5f3be322d7b3f58218512d0e0a16eafa818a77bd
davidrugeroehb <david.rugero@student.ehb.be>
2026-04-22 16:29:20 +0200

    update 22/04
    sessie pagina gemaakt volgens de PM, nav balk geupdate, registratiepagina,

--- modules/custom/shift_bezoeker/src/Controller/BezoekerController.php
+++ modules/custom/shift_bezoeker/src/Controller/BezoekerController.php
@@ -6,49 +6,77 @@ use Drupal\Core\Controller\ControllerBase;
 
 class BezoekerController extends ControllerBase {
 
-  /**
-   * Pagina voor het sessie overzicht.
-   */
   public function sessionsPage() {
-    $open_sessions = [
-      ['sessionName' => 'AI & Design', 'startTime' => '10:00', 'location' => 'Zaal A'],
-      ['sessionName' => 'Future of Tech', 'startTime' => '13:00', 'location' => 'Main Stage'],
+    // We gebruiken nu hele uren voor de linker tijdsas-kolom
+    $time_slots = [
+      '10:00' => '10:00',
+      '11:00' => '11:00',
+      '12:00' => '12:00',
+      '13:00' => '13:00',
+      '14:00' => '14:00',
+      '15:00' => '15:00',
+      '16:00' => '16:00',
     ];
 
-    $my_sessions = [
-      ['sessionName' => 'Sustainability in Code', 'startTime' => '15:30', 'location' => 'Zaal B'],
+    $stages = [
+      'main' => 'Main Stage',
+      'zaal_a' => 'Zaal A',
+      'zaal_b' => 'Zaal B',
+    ];
+
+    // Let op: de keys hier (zoals '10:00') moeten overeenkomen met de keys in $time_slots
+    $grid_data = [
+      'main' => [
+        '13:00' => [
+          'id' => 1, // Voeg deze ID's toe
+          'title' => 'Future of Tech',
+          'time' => '13:00 - 14:30',
+          'type' => 'open'
+        ]
+      ],
+      'zaal_a' => [
+        '10:00' => [
+          'id' => 2,
+          'title' => 'AI & Design',
+          'time' => '10:00 - 11:30',
+          'type' => 'open'
+        ]
+      ],
+      'zaal_b' => [
+        '15:00' => [
+          'id' => 3,
+          'title' => 'Sustainability in Code',
+          'time' => '15:30 - 17:00',
+          'type' => 'registered' // Deze zal dus paars worden in je grid!
+        ]
+      ],
     ];
 
     return [
       '#theme' => 'sessie_overzicht_template',
-      '#open_sessions' => $open_sessions,
-      '#my_sessions' => $my_sessions,
-      '#attached' => [
-        'library' => ['shift_theme/global-styling'],
-      ],
+      '#current_date' => '22 April 2026',
+      '#day_number' => '01',
+      '#time_slots' => $time_slots,
+      '#stages' => $stages,
+      '#grid_data' => $grid_data,
     ];
   }
 
-  /**
-   * Pagina voor de accountgegevens.
-   */
   public function accountPage() {
-    $user_data = [
-      'firstName' => 'Liam',
-      'lastName' => 'Stammeleer',
-      'email' => 'liam.stammeleer@example.com',
-      'phone' => '+32 470 00 00 00',
-      'company' => 'Erasmus',
-      'role' => 'Bezoeker',
-    ];
-
     return [
       '#theme' => 'account_gegevens_template',
-      '#user' => $user_data,
-      '#attached' => [
-        'library' => ['shift_theme/global-styling'],
+      '#user' => [
+        'firstName' => 'Bezoeker',
+        'lastName' => 'Naam',
+        'email' => 'test@shift.be',
+        'company' => 'Shift Festival',
       ],
     ];
   }
-
+  public function inschrijven($session_id) {
+    return [
+      '#theme' => 'inschrijving_bevestigd',
+      '#session_id' => $session_id,
+    ];
+  }
 }
