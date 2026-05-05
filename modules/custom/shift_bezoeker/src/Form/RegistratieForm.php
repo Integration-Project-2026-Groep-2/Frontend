90cf236467c78296d86584164ca43c7fd9ec22f4
davidrugeroehb <david.rugero@student.ehb.be>
2026-05-05 21:52:23 +0200

    form aangepast
    resolved

--- modules/custom/shift_bezoeker/src/Form/RegistratieForm.php
+++ modules/custom/shift_bezoeker/src/Form/RegistratieForm.php
@@ -24,10 +24,10 @@ class RegistratieForm extends FormBase {
     // 1. Keuze type registratie
     $form['registratie_type'] = [
       '#type' => 'radios',
-      '#title' => 'Registreer als:',
+      '#title' => $this->t('Registreer als:'),
       '#options' => [
-        'bezoeker' => 'Bezoeker',
-        'bedrijf' => 'Bedrijf',
+        'bezoeker' => $this->t('Bezoeker'),
+        'bedrijf' => $this->t('Bedrijf'),
       ],
       '#default_value' => 'bezoeker',
     ];
@@ -43,7 +43,7 @@ class RegistratieForm extends FormBase {
     ];
 
     $form['bezoeker_wrapper']['persoonlijk_header'] = [
-      '#markup' => '<h2 class="form-section-title">Persoonlijke Gegevens</h2>',
+      '#markup' => '<h2 class="form-section-title">' . $this->t('Persoonlijke Gegevens') . '</h2>',
     ];
 
     $form['bezoeker_wrapper']['name_row'] = [
@@ -53,29 +53,29 @@ class RegistratieForm extends FormBase {
 
     $form['bezoeker_wrapper']['name_row']['firstName'] = [
       '#type' => 'textfield',
-      '#title' => 'First Name',
+      '#title' => $this->t('First Name'),
       '#states' => ['required' => [':input[name="registratie_type"]' => ['value' => 'bezoeker']]],
     ];
 
     $form['bezoeker_wrapper']['name_row']['lastName'] = [
       '#type' => 'textfield',
-      '#title' => 'Last Name',
+      '#title' => $this->t('Last Name'),
       '#states' => ['required' => [':input[name="registratie_type"]' => ['value' => 'bezoeker']]],
     ];
 
     $form['bezoeker_wrapper']['email'] = [
       '#type' => 'email',
-      '#title' => 'Email Address',
+      '#title' => $this->t('Email Address'),
       '#states' => ['required' => [':input[name="registratie_type"]' => ['value' => 'bezoeker']]],
     ];
 
     $form['bezoeker_wrapper']['phone'] = [
       '#type' => 'tel',
-      '#title' => 'Phone (Optional)',
+      '#title' => $this->t('Phone (Optional)'),
     ];
 
     $form['bezoeker_wrapper']['event_header'] = [
-      '#markup' => '<h2 class="form-section-title">Event Details</h2>',
+      '#markup' => '<h2 class="form-section-title">' . $this->t('Event Details') . '</h2>',
     ];
 
     $form['bezoeker_wrapper']['event_row'] = [
@@ -85,18 +85,18 @@ class RegistratieForm extends FormBase {
 
     $form['bezoeker_wrapper']['event_row']['role'] = [
       '#type' => 'select',
-      '#title' => 'Rol',
+      '#title' => $this->t('Rol'),
       '#options' => [
-        'visitor' => 'Bezoeker',
-        'speaker' => 'Spreker',
-        'staff' => 'Medewerker',
+        'visitor' => $this->t('Bezoeker'),
+        'speaker' => $this->t('Spreker'),
+        'staff' => $this->t('Medewerker'),
       ],
-      '#empty_option' => 'Kies een rol...',
+      '#empty_option' => $this->t('Kies een rol...'),
     ];
 
-    $form['bezoeker_wrapper']['gdprConsent'] = [
+    $form['bezoeker_wrapper']['gdpr_consent'] = [
        '#type' => 'checkbox',
-       '#title' => 'I agree to the GDPR terms',
+       '#title' => $this->t('I agree to the GDPR terms'),
     ];
 
     // --- BEDRIJF WRAPPER ---
@@ -110,7 +110,7 @@ class RegistratieForm extends FormBase {
     ];
 
     $form['bedrijf_wrapper']['company_header'] = [
-      '#markup' => '<h2 class="form-section-title">Bedrijfsgegevens</h2>',
+      '#markup' => '<h2 class="form-section-title">' . $this->t('Bedrijfsgegevens') . '</h2>',
     ];
 
     $form['bedrijf_wrapper']['company_row'] = [
@@ -120,18 +120,18 @@ class RegistratieForm extends FormBase {
 
     $form['bedrijf_wrapper']['company_row']['companyName'] = [
       '#type' => 'textfield',
-      '#title' => 'Company Name',
+      '#title' => $this->t('Company Name'),
       '#states' => ['required' => [':input[name="registratie_type"]' => ['value' => 'bedrijf']]],
     ];
 
     $form['bedrijf_wrapper']['company_row']['vatNumber'] = [
       '#type' => 'textfield',
-      '#title' => 'VAT Number',
+      '#title' => $this->t('VAT Number'),
       '#states' => ['required' => [':input[name="registratie_type"]' => ['value' => 'bedrijf']]],
     ];
 
     $form['bedrijf_wrapper']['address_header'] = [
-      '#markup' => '<h2 class="form-section-title">Adresgegevens</h2>',
+      '#markup' => '<h2 class="form-section-title">' . $this->t('Adresgegevens') . '</h2>',
     ];
 
     $form['bedrijf_wrapper']['address_row'] = [
@@ -141,18 +141,18 @@ class RegistratieForm extends FormBase {
 
     $form['bedrijf_wrapper']['address_row']['street'] = [
       '#type' => 'textfield',
-      '#title' => 'Street',
+      '#title' => $this->t('Street'),
     ];
 
     $form['bedrijf_wrapper']['address_row']['city'] = [
       '#type' => 'textfield',
-      '#title' => 'City',
+      '#title' => $this->t('City'),
     ];
 
     // Submit knop
     $form['submit'] = [
       '#type' => 'submit',
-      '#value' => 'Registreer!',
+      '#value' => $this->t('Registreer!'),
       '#attributes' => ['class' => ['btn-primary']],
     ];
 
@@ -160,45 +160,45 @@ class RegistratieForm extends FormBase {
   }
 
   /**
-     * {@inheritdoc}
-     */
-    public function validateForm(array &$form, FormStateInterface $form_state) {
-      $values = $form_state->getValues();
-      $type = $values['registratie_type'];
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+    $type = $values['registratie_type'];
 
-      // 1. Validatie voor Bezoekers
-      if ($type === 'bezoeker') {
-        if (empty($values['firstName'])) {
-          $form_state->setErrorByName('firstName', 'Voornaam is verplicht.');
-        }
-        if (empty($values['lastName'])) {
-          $form_state->setErrorByName('lastName', 'Achternaam is verplicht.');
-        }
-        if (empty($values['email'])) {
-          $form_state->setErrorByName('email', 'E-mailadres is verplicht.');
-        }
+    // 1. Validatie voor Bezoekers
+    if ($type === 'bezoeker') {
+      if (empty($values['firstName'])) {
+        $form_state->setErrorByName('firstName', $this->t('Voornaam is verplicht.'));
       }
-
-      // 2. Validatie voor Bedrijven
-      if ($type === 'bedrijf') {
-        if (empty($values['companyName'])) {
-          $form_state->setErrorByName('companyName', 'Bedrijfsnaam is verplicht.');
-        }
-        if (empty($values['vatNumber'])) {
-          $form_state->setErrorByName('vatNumber', 'BTW-nummer is verplicht.');
-        }
-        // Jouw bestaande BTW-check
-        elseif (!str_starts_with(strtoupper($values['vatNumber']), 'BE')) {
-          $form_state->setErrorByName('vatNumber', 'Een Belgisch BTW-nummer moet beginnen met BE.');
-        }
+      if (empty($values['lastName'])) {
+        $form_state->setErrorByName('lastName', $this->t('Achternaam is verplicht.'));
+      }
+      if (empty($values['email'])) {
+        $form_state->setErrorByName('email', $this->t('E-mailadres is verplicht.'));
       }
     }
+
+    // 2. Validatie voor Bedrijven
+    if ($type === 'bedrijf') {
+      if (empty($values['companyName'])) {
+        $form_state->setErrorByName('companyName', $this->t('Bedrijfsnaam is verplicht.'));
+      }
+      if (empty($values['vatNumber'])) {
+        $form_state->setErrorByName('vatNumber', $this->t('BTW-nummer is verplicht.'));
+      }
+      // Jouw bestaande BTW-check
+      elseif (!str_starts_with(strtoupper($values['vatNumber']), 'BE')) {
+        $form_state->setErrorByName('vatNumber', $this->t('Een Belgisch BTW-nummer moet beginnen met BE.'));
+      }
+    }
+  }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    \Drupal::messenger()->addStatus('Bedankt! Je gegevens zijn verwerkt.');
+    \Drupal::messenger()->addStatus($this->t('Bedankt! Je gegevens zijn verwerkt.'));
     $form_state->setRedirect('shift_bezoeker.sessions'); // Of je eigen succes pagina
   }
 }
