f83164845b7580ffeeb5a38c613351abee371d30
Bouchta Bilal <bilal.bouchta@student.ehb.be>
2026-05-13 18:41:48 +0200

    fix: gebruik require ipv require_once voor autoloader in bootstrap
    require_once geeft true terug als bestand al geladen is, waardoor
    addPsr4() faalt. require geeft altijd het ClassLoader object terug.

--- tests_bootstrap.php
+++ tests_bootstrap.php
@@ -7,7 +7,7 @@ if (!defined('DRUPAL_ROOT')) {
   define('DRUPAL_ROOT', __DIR__ . '/web');
 }
 
-$loader = require_once __DIR__ . '/vendor/autoload.php';
+$loader = require __DIR__ . '/vendor/autoload.php';
 
 // Drupal's test-klassen (UnitTestCase, KernelTestBase, ...) zitten in
 // web/core/tests/Drupal/ en worden normaal via autoload-dev geregistreerd.
