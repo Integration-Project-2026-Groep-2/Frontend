3063b03e3a68dfc400537c3bdda4af160f06b504
Bouchta Bilal <bilal.bouchta@student.ehb.be>
2026-05-13 15:59:06 +0200

    test(session_management): voeg unit tests toe voor SessionListRequest en SessionListResponse

---
+++ modules/custom/Session_Management/tests/src/Unit/RabbitMQ/Message/SessionListRequestTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\Session_Management\Unit\RabbitMQ\Message;
+
+use Drupal\Session_Management\RabbitMQ\Message\SessionListRequest;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @coversDefaultClass \Drupal\Session_Management\RabbitMQ\Message\SessionListRequest
+ * @group session_management
+ */
+class SessionListRequestTest extends UnitTestCase {
+
+  public function testRoutingKeyIsCorrect(): void {
+    $msg = new SessionListRequest();
+    $this->assertSame('session.list.request', $msg->getRoutingKey());
+  }
+
+  public function testGetTypeIsCorrect(): void {
+    $msg = new SessionListRequest();
+    $this->assertSame('SessionListRequest', $msg->getType());
+  }
+
+  public function testToXmlProducesValidXml(): void {
+    $msg = new SessionListRequest();
+    $xml = $msg->toXml();
+
+    $dom = new \DOMDocument();
+    $this->assertTrue($dom->loadXML($xml), 'toXml() moet geldige XML produceren');
+    $this->assertSame('SessionListRequest', $dom->documentElement->localName);
+  }
+
+  public function testToXmlBevattRequestId(): void {
+    $msg = new SessionListRequest();
+    $dom = new \DOMDocument();
+    $dom->loadXML($msg->toXml());
+
+    $requestId = $dom->getElementsByTagName('requestId')->item(0);
+    $this->assertNotNull($requestId, '<requestId> element moet aanwezig zijn');
+    $this->assertStringStartsWith('req_', $requestId->textContent);
+  }
+
+  public function testToXmlBevattTimestamp(): void {
+    $msg = new SessionListRequest();
+    $dom = new \DOMDocument();
+    $dom->loadXML($msg->toXml());
+
+    $timestamp = $dom->getElementsByTagName('timestamp')->item(0);
+    $this->assertNotNull($timestamp, '<timestamp> element moet aanwezig zijn');
+    $this->assertNotEmpty($timestamp->textContent);
+  }
+
+  public function testRequestIdIsUniekPerAanroep(): void {
+    $msg = new SessionListRequest();
+    $dom1 = new \DOMDocument();
+    $dom1->loadXML($msg->toXml());
+    $id1 = $dom1->getElementsByTagName('requestId')->item(0)->textContent;
+
+    $dom2 = new \DOMDocument();
+    $dom2->loadXML($msg->toXml());
+    $id2 = $dom2->getElementsByTagName('requestId')->item(0)->textContent;
+
+    $this->assertNotSame($id1, $id2, 'Elk verzoek moet een unieke requestId hebben');
+  }
+
+}
